e0e8b63f20ae21a1a5bcdcc0f67a6e9ab9079a78
LowerDeckBoy <pa.krawiec@onet.pl>
2025-03-14 19:53:22 +0100

    [ADD] Quite ugly but gets the job done. Handling Win32 FileOpenDialog.

---
+++ Source/Platform/FileDialog.cpp
@@ -0,0 +1,118 @@
+#include <string>
+#include "Platform.hpp"
+#include <shobjidl.h>
+#include "FileDialog.hpp"
+#include <vector>
+
+namespace Luden::Platform
+{
+	static std::string NarrowStringDown(std::wstring Text)
+	{
+		std::string narrowed{};
+		narrowed.resize(Text.length());
+
+		wcstombs_s(nullptr, &narrowed[0], narrowed.size() + 1, Text.data(), Text.size());
+		
+		return narrowed;
+	}
+
+	static std::vector<COMDLG_FILTERSPEC> ExtensionFilterToTypes(EExtensionFilter Extension)
+	{
+		switch (Extension)
+		{
+		case Luden::Platform::EExtensionFilter::Scene:
+		{
+			return std::vector<COMDLG_FILTERSPEC>{
+				{ Filters::JSON,	L"*.json"	},
+				{ Filters::Any,		L"*.*"		}
+			};
+		}
+		case Luden::Platform::EExtensionFilter::Model:
+		{
+			return std::vector<COMDLG_FILTERSPEC>{
+				{ Filters::Primary,	L"*.gltf;*.glb;*.fbx;*.obj"	},
+				{ Filters::GLTF,	L"*.gltf;*.glb"				},
+				{ Filters::FBX,		L"*.fbx"					},
+				{ Filters::OBJ,		L"*.obj"					},
+				{ Filters::Any,		L"*.*"						}
+			};
+		}
+		case Luden::Platform::EExtensionFilter::Image:
+		{
+			return std::vector<COMDLG_FILTERSPEC>{
+				{ Filters::Primary,		L"*.jpg;*.jpeg;*.png;*.dds" },
+				{ Filters::JPG,			L"*.jpg;*.jpeg"				},
+				{ Filters::PNG,			L"*.png"					},
+				{ Filters::DDS,			L"*.dds"					},
+				{ Filters::Any,			L"*.*"						}
+			};
+		}
+		case Luden::Platform::EExtensionFilter::Any: [[fallthrough]];
+		default:
+		{
+			return std::vector<COMDLG_FILTERSPEC>{
+				{ Filters::Any,		L"*.*" }
+			};
+		}
+		}
+	}
+	
+	std::string FileDialog::Open(FOpenDialogOptions Options)
+	{
+		IFileOpenDialog* pFileOpen{};
+
+		// Create the FileOpenDialog object.
+		HRESULT hResult = ::CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL, IID_PPV_ARGS(&pFileOpen));
+
+		std::string output = "";
+
+		const auto extensions = ExtensionFilterToTypes(Options.FilterExtensions);
+
+		pFileOpen->SetFileTypes(static_cast<uint32_t>(extensions.size()), extensions.data());
+		pFileOpen->SetOptions(FOS_DONTADDTORECENT | FOS_FILEMUSTEXIST);
+		pFileOpen->SetTitle(std::wstring(Options.Title.begin(), Options.Title.end()).c_str());
+
+		std::wstring defaultFolderLocation = std::wstring(Options.OpenLocation.begin(), Options.OpenLocation.end());
+		IShellItem2* pDefaultFolder = nullptr;
+		::SHCreateItemFromParsingName(defaultFolderLocation.c_str(), NULL, IID_PPV_ARGS(&pDefaultFolder));
+
+		pFileOpen->SetFolder(pDefaultFolder);
+
+		if (SUCCEEDED(hResult))
+		{
+			// Show the Open dialog box.
+			hResult = pFileOpen->Show(nullptr);
+
+			// TODO:
+			//if (hResult == HRESULT_FROM_WIN32(ERROR_CANCELLED)) // No items were selected.
+			//{	
+			//	return output;
+			//}
+
+			// Get the file name from the dialog box.
+			if (SUCCEEDED(hResult))
+			{
+				IShellItem* pItem;
+				hResult = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hResult))
+				{
+					PWSTR pszFilePath{};
+					hResult = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+					if (SUCCEEDED(hResult))
+					{
+						output = NarrowStringDown(pszFilePath);
+
+						::CoTaskMemFree(pszFilePath);
+						pItem->Release();
+					}
+
+				}
+			}
+			pFileOpen->Release();
+
+		}
+
+		return output;
+	}
+} // namespace Luden::Platform
---
+++ Source/Platform/FileDialog.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "Export.hpp"
+
+namespace Luden::Platform
+{
+	// Rename to *Extensions*?
+	// Should these be maps?
+	namespace Filters
+	{
+		// Used for top-most filter.
+		// Groups all extensions for given output into single filter.
+		// Excludes *Any* filter.
+		constexpr LPCWSTR Primary	= L"";
+
+		constexpr LPCWSTR JSON	= L"JSON";
+
+		constexpr LPCWSTR GLTF	= L"glTF 2.0";
+		constexpr LPCWSTR FBX	= L"FBX";
+		constexpr LPCWSTR OBJ	= L"Wavefront";
+
+		constexpr LPCWSTR JPG	= L"JPG";
+		constexpr LPCWSTR PNG	= L"PNG";
+		constexpr LPCWSTR DDS	= L"DDS";
+
+		// Used as last filter.
+		// Accepts all extensions.
+		constexpr LPCWSTR Any	= L"All";
+		
+	}
+
+	enum class EExtensionFilter
+	{
+		Scene,
+		Model,
+		Image,
+		Any
+	};
+
+	struct FOpenDialogOptions
+	{
+		EExtensionFilter FilterExtensions = EExtensionFilter::Any;
+
+		// Default location for this dialog.
+		std::string	OpenLocation;
+		std::string	Title = "Select file";
+
+	};
+	
+	// TODO:
+	struct FSaveDialogOptions
+	{
+
+	};
+
+	class PLATFORM_API FileDialog
+	{
+	public:
+
+		static std::string Open(FOpenDialogOptions Options);
+
+		// TODO:
+		//static std::string Save();
+
+	};
+} // namespace Luden::Platform
