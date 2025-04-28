5aed375bd7b4551b7783626ab75080cfa1438733
LowerDeckBoy <pa.krawiec@onet.pl>
2025-04-28 19:12:02 +0200

    [MOD] Moving some functionality to Scene class.

--- Source/Engine/Scene/Scene.cpp
+++ Source/Engine/Scene/Scene.cpp
@@ -1,18 +1,31 @@
 #include "Asset/AssetImporter.hpp"
+#include "Renderer/Renderer.hpp"
 #include "Scene.hpp"
 
 namespace Luden
 {
     Scene::Scene()
+        : m_World(new World())
     {
-        m_World = new World();
+    }
+
+    Scene::Scene(AssetImporter* pAssetImporter)
+        : m_World(new World()), m_AssetImporter(pAssetImporter)
+    {
+        
+    }
+
+    Scene::Scene(AssetImporter* pAssetImporter, D3D12Device* pDevice)
+        : m_World(new World()), m_AssetImporter(pAssetImporter)
+    {
+        m_AssetImporter->Device = pDevice;
     }
 
     Scene::~Scene()
     {
         for (auto& model : Models)
         {
-            model.Release();
+            model->Release();
         }
 
         if (m_World)
@@ -21,12 +34,32 @@ namespace Luden
         }
     }
 
-    void Scene::Draw()
+    void Scene::Build(D3D12Device* pDevice)
     {
-        if (Models.empty())
+        if (IsEmpty())
         {
+            LOG_WARNING("Empty scene cannot be built.");
+
             return;
         }
+        
+        for (auto& model : Models)
+        {
+            model->Create(pDevice);
+        }
+
+    }
+
+    void Scene::Release()
+    {
+        for (auto& model : Models)
+        {
+            model->Release();
+        }
+
+        Models.clear();
+
+        GetRegistry()->clear();
     }
 
     void Scene::CreateEntity(Entity& Target)
@@ -34,16 +67,32 @@ namespace Luden
         m_World->CreateEntity(&Target);
     }
 
-    void Scene::AddModel(AssetImporter* pAssetImporter, Filepath Path)
+    bool Scene::AddModel(Filepath Path)
     {
         Model model{};
+        
+        if (!m_AssetImporter->ImportStaticMesh(Path, model))
+        {
+            return false;
+        }
+
         GetWorld()->CreateEntity(&model);
 
         model.AddComponent<ecs::NameComponent>(File::GetFilename(Path));
         model.AddComponent<ecs::TransformComponent>();
+        model.SetFilepath(Path);
 
-        pAssetImporter->ImportStaticMesh(Path, model);
+        model.Create(m_AssetImporter->Device);
 
-        Models.push_back(model);
+        Models.push_back(std::make_unique<Model>(model));
+
+        return true;
     }
+
+    bool Scene::IsEmpty() const
+    {
+        return Models.empty();
+    }
+
+
 } // namespace Luden
--- Source/Engine/Scene/Scene.hpp
+++ Source/Engine/Scene/Scene.hpp
@@ -5,52 +5,66 @@
 #include "SceneCamera.hpp"
 #include "Graphics/Model.hpp"
 #include "ECS/World.hpp"
-#include "SceneCamera.hpp"
+
 
 namespace Luden
 {
-	constexpr std::string_view SceneRootFolder	= "Scenes/";
-
-	// Note:
-	// Currently using hard-coded path to avoid copying Assets into project OutDir.
-	constexpr std::string_view ModelsRootFolder = "../../Assets/Models/";
-
 	class AssetImporter;
+	class Renderer;
 
+	// TODO:
+	// AssetImporter should be owned here.
+	// Pointer to RHI should too?
+	// Materials, Textures and Buffers should be stored inside a Scene?
+	// ^ that way it should be easier to create resource buffers and manage scene cleaning.
 	class Scene
 	{
 	public:
 		Scene();
+		Scene(AssetImporter* pAssetImporter);
+		Scene(AssetImporter* pAssetImporter, D3D12Device* pDevice);
 		~Scene();
-
-		//void Load();
-		//void Save();
-
-		// Draw all models in this scene.
-		void Draw();
 
 		std::string Name;
 
-		std::vector<Model> Models;
+		// Initialize Scene and create resources for scene content.
+		void Build(D3D12Device* pDevice);
+
+		// Release all the Scene resources.
+		// Clear World registry.
+		void Release();
 
 		void CreateEntity(Entity& Target);
 
-		void AddModel(AssetImporter* pAssetImporter, Filepath Path);
+		bool AddModel(Filepath Path);
 
-		World* GetWorld()
-		{
-			return m_World;
-		}
+		World*			GetWorld()		{ return m_World;			}
+		entt::registry* GetRegistry()	{ return m_World->Registry; }
 
-		entt::registry* GetRegistry()
-		{
-			return m_World->Registry;
-		}
+		bool IsEmpty() const;
+		
+
+		// TODOs:
+		//void AddPointLight();
+		//void AddDirectionalLight();
+		//void AddSpotLight();
+
+		// Test
+		// Scene resource.
+		// They gonna be used to create buffers for GPU driven rendering.
+		std::vector<std::unique_ptr<Model>> Models;
+		std::vector<Material> Materials;
+		std::vector<StaticMesh> Meshes;
+		std::vector<D3D12Texture*> Textures;
+
+		// Log scene stats, like amount of models, meshes, lights this scene has.
+		//void LogDebugInfo();
 
 	private:
 		Filepath m_Filepath;
 
 		World* m_World = nullptr;
+		AssetImporter* m_AssetImporter = nullptr;
 
 	};
 } // namespace Luden
