80cec7a119371bc8936271cc7daefbf50de1520d
LowerDeckBoy <pa.krawiec@onet.pl>
2025-05-16 19:50:56 +0200

    [MOD] Got it up and going.

--- Source/Editor/Panels/ContentPanel.cpp
+++ Source/Editor/Panels/ContentPanel.cpp
@@ -1,29 +1,26 @@
-#include <Core/File.hpp>
+#include "../Colors.hpp"
+#include "../Components/Tooltip.hpp"
+#include "../Editor.hpp"
 #include "ContentPanel.hpp"
-#include <ImGui/imgui.h>
-#include <ImGui/imgui_stdlib.h>
-#include <FontAwsome6/IconsFontAwesome6.h>
+#include <Core/File.hpp>
 
 
 namespace Luden::Panel
 {
 	// Temporal
 	std::string AssetDirectory = "../../Assets";
-	//std::string AssetDirectory = "Luden/Assets";
-	//const char* AssetDirectory = "Assets/";
 
 	ContentPanel::ContentPanel()
 		: m_CurrentDirectory(AssetDirectory)
 	{
 		m_CurrentDirectory = std::filesystem::current_path().parent_path().parent_path().append("Assets");
-		//m_CurrentDirectory += "Assets";
 	}
 
 	void ContentPanel::DrawPanel()
 	{
 		ImGui::Begin("Content Hierarchy");
 
-		std::string directoryString = m_CurrentDirectory.string();
+		const std::string& directoryString = m_CurrentDirectory.string();
 		ImGui::Text("Current directory: %s", directoryString.c_str());
 
 		ImGui::End();
@@ -32,69 +29,81 @@ namespace Luden::Panel
 
 		ImGui::PushStyleColor(ImGuiCol_Button, { 0.0f, 0.0f, 0.0f, 0.0f });
 
-		if (ImGui::Button("...", { 50, 50 }))
+		static float padding = 25.0f;
+		static float imageSize = 64.0f;
+		float cellSize = imageSize + padding;
+
+		const ImVec2 thumbnailSize = { imageSize, imageSize };
+
+		const auto& panelWidth = ImGui::GetContentRegionAvail().x;
+		int32 columnsCount = (int32)(panelWidth / cellSize);
+		if (columnsCount < 1.0f) columnsCount = 1;
+		
+		if (ImGui::BeginTable("Assets", columnsCount))
 		{
-			if (m_CurrentDirectory.has_parent_path())
+			ImGui::TableNextColumn();
+
+			gui::OnItemHover("Move to parent directory.");
+			ImGui::Button("...", thumbnailSize);
+			if (ImGui::IsItemHovered())
+			{
+				ImGui::PushStyleColor(ImGuiCol_Border, gui::Color::Gray);
+				ImGui::SetTooltip("Move to parent directory.");
+				ImGui::PopStyleColor();
+			}
+
+			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
 				m_CurrentDirectory = m_CurrentDirectory.parent_path();
-				
 			}
+			ImGui::Text("...");
+
+			ImGui::TableNextColumn();
+
+			for (const auto& path : std::filesystem::directory_iterator(Filepath(m_CurrentDirectory)))
+			{
+				std::string str = path.path().filename().string();
+
+				if (path.is_directory())
+				{
+					ImGui::ImageButton(str.c_str(), (ImTextureID)Luden::Editor::EditorDirectoryTexture->ShaderResourceHandle.GpuHandle.ptr, thumbnailSize);
+
+					if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
+					{
+						m_CurrentDirectory /= path.path().filename();
+					}
+
+					ImGui::Text(str.c_str());
+				}
+				else
+				{
+					ImGui::ImageButton(str.c_str(), (ImTextureID)Luden::Editor::EditorFileTexture->ShaderResourceHandle.GpuHandle.ptr, thumbnailSize);
+
+					if (ImGui::BeginDragDropSource())
+					{
+						std::string relative = AssetDirectory + "/";
+						relative.append(std::filesystem::relative(path, AssetDirectory).string());
+
+						// Without adding 1 to length some files cannot be loaded.
+						ImGui::SetDragDropPayload("PAYLOAD_ITEM", relative.data(), (relative.length() + 1) * sizeof(char), ImGuiCond_Once);
+	
+						ImGui::EndDragDropSource();
+						
+					}
+
+					ImGui::Text(str.c_str());
+					gui::OnItemHover(str);
+				}
+
+				ImGui::TableNextColumn();
+			}
+
+			ImGui::EndTable();
 		}
 		
-		ImGui::SameLine();
-
-		for (auto& path : std::filesystem::directory_iterator(Filepath(m_CurrentDirectory)))
-		{
-			std::string str = path.path().filename().string();
-			auto availableSize = ImGui::GetContentRegionAvail();
-
-			
-			if (path.is_directory())
-			{
-				if (availableSize.x - 50 > 55.0)
-				{
-					//ImGui::Text("%s", str.c_str());
-					//ImGui::PushID(str.c_str());
-					
-					//if (ImGui::Button(ICON_FA_FOLDER"", {50, 50}))
-					if (ImGui::Button(str.c_str(), { 50, 50 }))
-					{
-						m_CurrentDirectory /= path.path().filename();
-					}
-					
-					//ImGui::PopID();
-					ImGui::SameLine();
-				}
-				else
-				{
-					
-					//ImGui::Text("%s", str.c_str());
-					//ImGui::PushID(str.c_str());
-					if (ImGui::Button(str.c_str(), { 50, 50 }))
-					{
-						m_CurrentDirectory /= path.path().filename();
-					}
-					//ImGui::PopID();
-				}			
-			}
-			else
-			{
-				if (availableSize.x - 50 > 55.0)
-				{
-					ImGui::Button(str.c_str(), { 50, 50 });
-					ImGui::SameLine();
-				}
-				else
-				{
-					ImGui::Button(str.c_str(), { 50, 50 });
-				}
-				//ImGui::PushID(str.c_str());
-				//ImGui::Button(ICON_FA_FILE"", { 50, 50 });
-				//ImGui::PopID();
-			}
-		}
 		ImGui::PopStyleColor();
 
 		ImGui::End();
 	}
+
 } // namespace Luden::Panel
--- Source/Editor/Panels/ContentPanel.hpp
+++ Source/Editor/Panels/ContentPanel.hpp
@@ -2,8 +2,12 @@
 
 namespace Luden::Panel
 {
+	// TODO:
+	// - Make tree structure of project directories.
+	// - Determine display icons by file extensions.
 	class ContentPanel
 	{
+		friend class Editor;
 	public:
 		ContentPanel();
 		~ContentPanel() = default;
