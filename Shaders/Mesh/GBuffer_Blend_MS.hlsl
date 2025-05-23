e6209be4fd21a4a2639059af3049ca302b804a51
LowerDeckBoy <pa.krawiec@onet.pl>
2025-05-23 19:20:52 +0200

    [ADD] TODO NOW

---
+++ Shaders/Mesh/GBuffer_Blend_MS.hlsl
@@ -0,0 +1,215 @@
+#ifndef GBUFFER_MS_HLSL
+#define GBUFFER_MS_HLSL
+
+
+#include "../Material.hlsli"
+#include "../Mesh.hlsli"
+		
+#define AS_GROUP_SIZE 32
+
+#define GBUFFER_ROOT_SIG "RootFlags(CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED |"\
+	"DENY_HULL_SHADER_ROOT_ACCESS |"\
+	"DENY_DOMAIN_SHADER_ROOT_ACCESS |"\
+	"DENY_GEOMETRY_SHADER_ROOT_ACCESS),"\
+	"CBV(b0, space=0), "\
+	"RootConstants(num32BitConstants=8, b1), "\
+	"RootConstants(num32BitConstants=20, b2), "\
+	"StaticSampler(s0, "\
+		"addressU = TEXTURE_ADDRESS_WRAP, "\
+		"addressV = TEXTURE_ADDRESS_WRAP, "\
+		"filter = FILTER_MAXIMUM_ANISOTROPIC )"
+
+struct SceneConstants
+{
+	float4x4 View;
+	float4x4 Projection;
+};
+
+struct Transform
+{
+	row_major float4x4 WVP;
+	row_major float4x4 World;
+};
+
+// Indices to buffers.
+struct PushConstants
+{
+	uint VertexIndex;
+	uint MeshletIndex;
+	uint MeshletVerticesIndex;
+	uint MeshletTrianglesIndex;
+	uint MeshletBoundsIndex;
+	uint bDrawMeshlets;
+	uint bAlphaMask;
+	uint GBufferBaseColor;
+};
+
+struct Vertex
+{
+	float3 Position;
+	float2 TexCoord;
+	float3 Normal;
+	float3 Tangent;
+	float3 Bitangent;
+};
+
+struct VertexOut
+{
+	float4 Position : SV_POSITION;
+	float4 WorldPosition : WORLD_POSITION;
+	float2 TexCoord : TEXCOORD;
+	float3x3 TBN : TBN;
+	uint MeshletIndex : COLOR0;
+};
+
+ConstantBuffer<Transform> Transforms : register(b0);
+ConstantBuffer<PushConstants> Constants : register(b1);
+ConstantBuffer<FMaterial> Material : register(b2);
+
+VertexOut GetVertexAttributes(Vertex InVertex, uint MeshletIndex)
+{
+	VertexOut vout;
+	
+	vout.Position = mul(Transforms.WVP, float4(InVertex.Position, 1.0f));
+	vout.WorldPosition = mul(Transforms.World, float4(InVertex.Position, 1.0f));
+	vout.TexCoord = InVertex.TexCoord;
+	
+	float3 N = normalize(mul((float3x3) Transforms.World, InVertex.Normal));
+	float3 T = normalize(mul((float3x3) Transforms.World, InVertex.Tangent));
+	float3 B = normalize(mul((float3x3) Transforms.World, InVertex.Bitangent));
+
+	vout.TBN = float3x3(T, B, N);
+	vout.TBN = mul((float3x3) Transforms.World, transpose(vout.TBN));
+
+	vout.MeshletIndex = MeshletIndex;
+	
+	return vout;
+}
+
+struct Payload
+{
+	uint MeshletIndices[AS_GROUP_SIZE];
+};
+
+groupshared Payload sPayload;
+
+[NumThreads(AS_GROUP_SIZE, 1, 1)]
+void ASMain(
+	uint GroupThreadID : SV_GroupThreadID,
+	uint DispatchThreadID : SV_DispatchThreadID,
+	uint GroupID : SV_GroupID)
+{
+	sPayload.MeshletIndices[GroupThreadID] = DispatchThreadID;
+	
+	DispatchMesh(AS_GROUP_SIZE, 1, 1, sPayload);
+}
+
+[RootSignature(GBUFFER_ROOT_SIG)]
+[NumThreads(128, 1, 1)]
+[OutputTopology("triangle")]
+void MSMain(
+	uint GroupThreadID : SV_GroupThreadID,
+	uint GroupID : SV_GroupID,
+	in payload Payload payload,
+	out indices uint3 Triangles[MAX_TRIANGLES],
+	out vertices VertexOut Vertices[MAX_VERTICES])
+{
+	StructuredBuffer<Meshlet> Meshlets = ResourceDescriptorHeap[Constants.MeshletIndex];
+	StructuredBuffer<Vertex> VertexBuffer = ResourceDescriptorHeap[Constants.VertexIndex];
+	StructuredBuffer<uint> MeshletVertices = ResourceDescriptorHeap[Constants.MeshletVerticesIndex];
+	StructuredBuffer<uint> MeshletTriangles = ResourceDescriptorHeap[Constants.MeshletTrianglesIndex];
+	
+	uint meshletIndex = payload.MeshletIndices[GroupID];
+	Meshlet meshlet = Meshlets[meshletIndex];
+
+	SetMeshOutputCounts(meshlet.VertexCount, meshlet.TriangleCount);
+
+	if (GroupThreadID < meshlet.TriangleCount)
+	{
+		uint packed = MeshletTriangles.Load(meshlet.TriangleOffset + GroupThreadID);
+
+		Triangles[GroupThreadID] = UnpackTriangle(packed);
+	}
+	
+	if (GroupThreadID < meshlet.VertexCount)
+	{
+		uint vertexIndex = MeshletVertices[meshlet.VertexOffset + GroupThreadID];
+		Vertex vertex = VertexBuffer[vertexIndex];
+		
+		Vertices[GroupThreadID] = GetVertexAttributes(vertex, GroupID);
+	}
+
+}
+
+SamplerState AnisotropicSampler : register(s0);
+
+struct GBuffers
+{
+	float4 BaseColor;
+	float4 Normal;
+	float4 MetallicRoughness;
+	float4 Emissive;
+};
+
+int IsIndexValid(uint Index)
+{
+	if (Index == 0xFFFFFFFF)
+	{
+		return 0;
+	}
+
+	return 1;
+}
+
+Texture2D GetTexture(in uint Index)
+{
+	Texture2D output = ResourceDescriptorHeap[Index];
+	return output;
+}
+
+[RootSignature(GBUFFER_ROOT_SIG)]
+float4 PSMain(VertexOut pin) : SV_TARGET0
+{
+	float4 output = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	if (IsIndexValid(Material.EmissiveIndex))
+	{
+		Texture2D emissiveTexture = GetTexture(Material.EmissiveIndex);
+		
+		float4 emissiveColor = emissiveTexture.Sample(AnisotropicSampler, pin.TexCoord);
+		
+		output.rgb += emissiveColor.rgb;
+	}
+	if (IsIndexValid(Material.BaseColorIndex))
+	{
+		Texture2D baseColorTexture = GetTexture(Material.BaseColorIndex);
+		
+		//float4 baseColor = baseColorTexture.Sample(AnisotropicSampler, pin.TexCoord) * float4(Material.BaseColorFactor.rgba);
+		float4 baseColor = baseColorTexture.Sample(AnisotropicSampler, pin.TexCoord);
+		
+		//if (Constants.bAlphaMask)
+		//{
+		//	if (baseColor.a < 0.5)
+		//	{
+		//		discard;
+		//	}
+		//}
+		
+		if (baseColor.a  == 0)
+		{
+			discard;
+		}
+		
+		if (Constants.bDrawMeshlets)
+		{
+			float3 meshletColor = GetMeshletColorHashed(pin.MeshletIndex);
+			return float4(meshletColor, 1.0f);
+		}
+		
+		output = baseColor;
+	}
+	
+	return output.rgba;
+	
+}
+
+#endif // GBUFFER_MS_HLSL
