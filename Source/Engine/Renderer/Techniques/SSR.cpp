7846f1f0a9a4f7487e58d16566cc4345c7b98308
LowerDeckBoy <pa.krawiec@onet.pl>
2025-08-28 19:05:19 +0200

    [ADD] New

--- Source/Engine/Renderer/Techniques/SSR.cpp
+++ Source/Engine/Renderer/Techniques/SSR.cpp
@@ -1,6 +1,41 @@
 #include "SSR.hpp"
+#include "Asset/ShaderCompiler.hpp"
+#include "D3D12/D3D12Utility.hpp"
+#include "Scene/SceneCamera.hpp"
 
 namespace Luden
 {
+	SSR::SSR(D3D12RHI* pD3D12RHI, ShaderCompiler* pShaderCompiler, uint32 Width, uint32 Height)
+		: RenderPass(pD3D12RHI)
+	{
+		Pipeline.Compute = pShaderCompiler->CompileCS("../../Shaders/SSR/SSR.hlsl", true);
+		VERIFY_D3D12_RESULT(Pipeline.RootSignature.BuildFromShader(pD3D12RHI->Device, &Pipeline.Compute, PipelineType::Compute));
 
+		D3D12ComputePipelineStateBuilder builder;
+		builder.SetComputeShader(&Pipeline.Compute);
+		builder.SetRootSignature(&Pipeline.RootSignature);
+		VERIFY_D3D12_RESULT(builder.Build(pD3D12RHI->Device, Pipeline));
+
+	}
+
+	SSR::~SSR()
+	{
+	}
+
+	void SSR::Render(Frame* CurrentFrame, uint32 SceneImageIndex, uint32 NormalsIndex, uint32 RoughnessIndex, uint32 DepthIndex, SceneCamera* pCamera)
+	{
+		const auto renderBeginTime = Time::GetTimestamp();
+
+		auto commandList = CurrentFrame->ComputeCommandList;
+
+		RenderTime = Time::GetDurationInMiliseconds(renderBeginTime);
+	}
+
+	void SSR::Resize(uint32 Width, uint32 Height)
+	{
+	}
+
+	void SSR::Release()
+	{
+	}
 } // namespace Luden
--- Source/Engine/Renderer/Techniques/SSR.hpp
+++ Source/Engine/Renderer/Techniques/SSR.hpp
@@ -1,8 +1,38 @@
 #pragma once
 
+#include "../RenderPass.hpp"
+
 namespace Luden
 {
-	class SSR
+	class ShaderCompiler;
+	class SceneCamera;
+	
+	// https://github.com/mateeeeeee/Adria/blob/master/Adria/Resources/Shaders/CommonResources.hlsli
+	class SSR : public RenderPass
 	{
+	public:
+		SSR(D3D12RHI* pD3D12RHI, ShaderCompiler* pShaderCompiler, uint32 Width, uint32 Height);
+		~SSR();
+
+		void Render(Frame* CurrentFrame, uint32 SceneImageIndex, uint32 NormalsIndex, uint32 RoughnessIndex, uint32 DepthIndex, SceneCamera* pCamera);
+
+		void Resize(uint32 Width, uint32 Height) override;
+		void Release() override;
+
+		struct
+		{
+			DirectX::XMFLOAT4X4 View;
+
+			uint32 InputImageIndex;
+			uint32 OutputImageIndex;
+			uint32 NormalIndex;
+			uint32 RoughnessIndex;
+			uint32 DepthIndex;
+
+		} Parameters;
+
+
+	private:
+
 	};
 } // namespace Luden
