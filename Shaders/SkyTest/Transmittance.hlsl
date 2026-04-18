d617bd3b1172e9f78030e8809ee5f595c2fab7ce
LowerDeckBoy <pa.krawiec@onet.pl>
2026-04-18 19:16:08 +0200

    [MOD] Significant improvement but something is still off.

--- Shaders/SkyTest/Transmittance.hlsl
+++ Shaders/SkyTest/Transmittance.hlsl
@@ -7,97 +7,67 @@
 #define THREAD_GROUP_SIZE_X 16
 #define THREAD_GROUP_SIZE_Y 16
 
-//#define STEP_COUNT 1000
-#define STEP_COUNT 8
+#define STEP_COUNT 128
+//#define STEP_COUNT 40
 
 struct TransmittanceConstants
 {
 	uint TransmittanceIndex;
 };
 
-//ConstantBuffer<SkyParameters> Parameters : register(b0);
 ConstantBuffer<TransmittanceConstants> Constants : register(b0);
-
-float3 getSigmaS(float h)
-{
-	float3 rayleigh = RayleighScattering * exp(-h / RayleighHeight);
-	float mie = 3.996 * exp(-h / MieHeight);
-	return rayleigh + mie;
-}
 
 float3 getSigmaT(float h)
 {
-	float3 rayleigh = RayleighScattering * exp(-h / RayleighHeight);
-	float mie = (3.996 + 4.4) * exp(-h / MieHeight);
-	//float mie = (3.996) * exp(-h / MieHeight);
-	float3 ozone = (Nitrogen * 0.06f) * exp(-h / 8000);
-	//float3 ozone = Nitrogen * max(0.0f, 1 - 0.5 * abs(h - OzoneCenter) / OzoneThickness); 
-	
-	//float3 rayleigh = RayleighScattering * exp(-h / RayleighHeight);
-	//float3 rayleigh = RayleighScattering * GetRayleighHeightFactor(h);
-	//float mie = (3.996 + 4.4) * GetMieHeightFactor(h);
-	//float3 ozone = Nitrogen * GetOzoneHeightFactor(h);
-	//float3 ozone = Nitrogen * max(0.0f, 1 - abs(h - OzoneCenter) / OzoneThickness);
-	
-	return rayleigh + mie.xxx + ozone;
-}
+	float3	rayleigh	= RayleighScattering * exp(-h / RayleighHeight);
+	float	mie			= (MieScattering + MieAbsorption) * exp(-h / MieHeight);
+	float3  ozone		= OzoneAbsorption * max(0.0f, 1.0f - 0.5f * abs(h - OzoneCenter) / OzoneThickness);
 
-void getSigmaST(float h, out float3 sigmaS, out float3 sigmaT)
-{
-	float3 rayleigh = RayleighScattering * exp(-h / RayleighHeight);
-
-	float mieDensity = exp(-h / MieHeight);
-	float mieS = 3.996 * mieDensity;
-	float mieT = (3.996 + 4.4) * mieDensity;
-
-	float3 ozone = float3(0.65f, 1.881f, 0.085f) * max(
-        0.0f, 1 - 0.5 * abs(h - 25) / 30);
-
-	sigmaS = rayleigh + mieS;
-	sigmaT = rayleigh + mieT + ozone;
+	return rayleigh + mie + ozone;
 }
 
 // https://github.com/AirGuanZ/AtmosphereRenderer/blob/main/asset/transmittance.hlsl
+// https://github.com/elliahu/atmosphere/blob/main/shaders/transmittance.slang
 [RootSignature(SKY_ROOT)]
 [numthreads(THREAD_GROUP_SIZE_X, THREAD_GROUP_SIZE_Y, 1)]
 void CSMain(uint3 DispatchThreadID : SV_DispatchThreadID)
 {
 	RWTexture2D<float4> Transmittance = ResourceDescriptorHeap[Constants.TransmittanceIndex];
+	
 	uint width, height;
 	Transmittance.GetDimensions(width, height);
+	
 	if (DispatchThreadID.x >= width || DispatchThreadID.y >= height)
 	{
 		return;
 	}
 	
-	float2 texCoord = (float2(DispatchThreadID.xy) + 0.5f);
+	float2 texCoord = float2(DispatchThreadID.xy) + float2(0.5f, 0.5f);
 	
-	float theta = asin(lerp(-1.0, 1.0, texCoord.y / height));
-	float h = lerp(0.0f, AtmosphereRadius - EarthRadius, texCoord.x / width);
+	float theta = asin(lerp(-1.0f, 1.0f, texCoord.y / float(height)));
+	float h = lerp(0.0f, AtmosphereRadius - PlanetRadius, texCoord.x / float(width));
 
-	float2 o = float2(0, EarthRadius + h);
-	float2 d = float2(cos(theta), sin(theta));
+	float2 origin = float2(0, PlanetRadius + h);
+	float2 direction = float2(cos(theta), sin(theta));
 	
-
-	float t = 0;
-	if (!findClosestIntersectionWithCircle(o, d, AtmosphereRadius, t))
+	float t = 0.0f;
+	if (!findClosestIntersectionWithCircle(origin, direction, PlanetRadius, t))
 	{
-		bool temp = findClosestIntersectionWithCircle(o, d, EarthRadius, t);
+		bool temp = findClosestIntersectionWithCircle(origin, direction, AtmosphereRadius, t);
 	}
 	
-	float2 end = o + t * d;
+	float2 end = origin + t * direction;
 
-	float3 sum;
+	float3 sum = 0.0f;
 	for (int i = 0; i < STEP_COUNT; ++i)
 	{
-		//float2 pi = lerp(o, end, (i + 0.5f) / STEP_COUNT);
-		float2 pi = o + (i + 0.5f) * STEP_COUNT;
-		float hi = length(pi) + EarthRadius;
+		float2 pi = lerp(origin, end, float(i) / float(STEP_COUNT));
+		float hi = length(pi) - PlanetRadius;
 		float3 sigma = getSigmaT(hi);
 		sum += sigma;
 	}
 	
-	float3 result = exp(-sum * (t / STEP_COUNT));
+	float3 result = exp(-sum * (t / float(STEP_COUNT)));
 	
 	Transmittance[DispatchThreadID.xy] = float4(result, 1.0f);
 }
